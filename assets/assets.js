ce75a714e4430b59b1b03640a93d10e051b5cdeb
katarzynasornat <katarzyna.staniewicz0504@gmail.com>
2025-03-28 15:03:46 +0100

    changing path in next.config.mjs

--- assets/assets.js
+++ assets/assets.js
@@ -86,22 +86,22 @@ export const workData = [
     {
         title: 'Frontend project',
         description: 'Web Design',
-        bgImage: '/assets/public/work-1.png',
+        bgImage: '${basePath}/assets/public/work-1.png',
     },
     {
         title: 'Geo based app',
         description: 'Mobile App',
-        bgImage: '/assets/public/work-2.png',
+        bgImage: '${basePath}/assets/public/work-2.png',
     },
     {
         title: 'Photography site',
         description: 'Web Design',
-        bgImage: '/assets/public/work-3.png',
+        bgImage: '${basePath}/assets/public/work-3.png',
     },
     {
         title: 'UI/UX designing',
         description: 'UI/UX Design',
-        bgImage: '/assets/public/work-4.png',
+        bgImage: '${basePath}/assets/public/work-4.png',
     },
 ]
 
