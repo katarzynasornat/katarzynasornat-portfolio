706a19499e31832f5c4eca6b0754ec5ad2d97c52
katarzynasornat <katarzyna.staniewicz0504@gmail.com>
2025-03-28 14:46:53 +0100

    update

--- assets/assets.js
+++ assets/assets.js
@@ -86,22 +86,22 @@ export const workData = [
     {
         title: 'Frontend project',
         description: 'Web Design',
-        bgImage: '/work-1.png',
+        bgImage: '/assets/public/work-1.png',
     },
     {
         title: 'Geo based app',
         description: 'Mobile App',
-        bgImage: '/work-2.png',
+        bgImage: '/assets/public/work-2.png',
     },
     {
         title: 'Photography site',
         description: 'Web Design',
-        bgImage: '/work-3.png',
+        bgImage: '/assets/public/work-3.png',
     },
     {
         title: 'UI/UX designing',
         description: 'UI/UX Design',
-        bgImage: '/work-4.png',
+        bgImage: '/assets/public/work-4.png',
     },
 ]
 
